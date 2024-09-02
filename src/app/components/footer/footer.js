ec7796975c3d3c0598f5d50b56e7af56dfa085ab
MickaelCarlucci <frewmike17@gmail.com>
2024-09-02 10:34:34 +0200

    fix apos

--- src/app/components/footer/footer.js
+++ src/app/components/footer/footer.js
@@ -7,10 +7,10 @@ export default function Footer() {
     <div className="footer">
       <div>
         <p className="bold">
-          Site du syndicat CGT élu dans l'entreprise Teleperformance
+          Site du syndicat CGT élu dans l&apos;entreprise Teleperformance
         </p>
         <p>Siège: 263 rue de Paris 93100 Montreuil.</p>
-        <p>Nom et prénom d'un représentant de la fédé</p>
+        <p>Nom et prénom d&apos;un représentant de la fédé</p>
         <p>SIREN: 77567845100060</p>
       </div>
 
@@ -27,9 +27,9 @@ export default function Footer() {
 
       <div>
         Les contenus du site [Nom du site] (textes, images, logos, etc.) sont la
-        propriété exclusive du Syndicat CGT de l'entreprise Teteperformance.
+        propriété exclusive du Syndicat CGT de l&apos;entreprise Teteperformance.
         Toute reproduction totale ou partielle de ces éléments est interdite
-        sans l'autorisation préalable du syndicat.
+        sans l&apos;autorisation préalable du syndicat.
       </div>
 
       <div>
@@ -41,18 +41,18 @@ export default function Footer() {
           électroniques enregistrées servent uniquement à vous notifier les
           nouveaux documents disponibles sur le site ou les sondages nécessitant
           votre avis). En aucun cas, vos données ne sont utilisées ou revendues
-          à des fins commerciales. Vous disposez d'un droit d'accès, de
+          à des fins commerciales. Vous disposez d&apos;un droit d&apos;accès, de
           rectification et de suppression de vos données personnelles. Pour
           exercer ces droits, vous pouvez consulter votre profil directement sur
-          le site ou contacter Didier Thiebauld à l'adresse suivante : [email de
+          le site ou contacter Didier Thiebauld à l&apos;adresse suivante : [email de
           contact].
         </p>
-        <p>Ce site n'utilise pas de cookie qui exploite vos données.</p>
+        <p>Ce site n&apos;utilise pas de cookie qui exploite vos données.</p>
       </div>
 
       <div>
         <p>
-          Si vous souhaitez contacter l'un de vos représentants n'hésitez pas à
+          Si vous souhaitez contacter l&apos;un de vos représentants n&apos;hésitez pas à
           vous diriger vers la rubrique <Link href="/contact">contact</Link>
         </p>
       </div>
