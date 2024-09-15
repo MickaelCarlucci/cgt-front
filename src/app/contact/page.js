237b84aa740636d3555d6921312da05e40810953
MickaelCarlucci <frewmike17@gmail.com>
2024-09-15 13:49:03 +0200

    add template new page

---
+++ src/app/contact/page.js
@@ -0,0 +1,17 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import "./page.css";
+
+export default function Page() {
+
+
+  return (
+   <>
+   <div>
+      
+    </div>
+    </>
+  )
+}
