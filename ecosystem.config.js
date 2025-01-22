771d1d5f0671206fd0b1ce185c88ebc25b3ca507
MickaelCarlucci <frewmike17@gmail.com>
2025-01-22 11:50:00 +0100

    add ecosystem file for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "frontend",
+      script: "npm",
+      args: "start",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000,
+      },
+    },
+  ],
+};
