1f7083a3816c611821e236c02952f5f0be554229
MickaelCarlucci <frewmike17@gmail.com>
2025-02-02 09:59:38 +0100

    change next config rules

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'http',  // Ou 'https' si vous utilisez https en production
-          hostname: 'localhost',
-          port: '3003',  // Spécifiez le port si nécessaire
-          pathname: '/images/**',  // Autorise les images dans ce chemin
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http", // Ou 'https' si vous utilisez HTTPS en production
+        hostname: "localhost",
+        port: "3003", // Spécifiez le port si nécessaire
+        pathname: "/images/**", // Autorise les images dans ce chemin
+      },
+    ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/uploads/:path*", // Route vers les fichiers PDF
+        destination: "http://88.223.95.21/uploads/:path*", // Redirection vers le serveur
+      },
+    ];
+  },
+};
+
+export default nextConfig;
