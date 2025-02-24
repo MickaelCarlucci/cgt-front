8dc2f87d6922b276387fb5419f9dbb7c276ea471
MickaelCarlucci <frewmike17@gmail.com>
2025-02-24 20:38:53 +0100

    .

--- next.config.mjs
+++ next.config.mjs
@@ -3,21 +3,17 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http", // Ou 'https' si vous utilisez HTTPS en production
+        protocol: "http",
         hostname: "localhost",
-        port: "3003", // Spécifiez le port si nécessaire
+        port: "3003",
+        pathname: "/images/**",
+      },
+      {
+        protocol: "https", // Utilise HTTPS pour la production
+        hostname: "cgt-tp.fr",
         pathname: "/images/**", // Autorise les images dans ce chemin
       },
     ],
-  },
-
-  async rewrites() {
-    return [
-      {
-        source: "/uploads/:path*", // Route vers les fichiers PDF
-        destination: "http://88.223.95.21/uploads/:path*", // Redirection vers le serveur
-      },
-    ];
   },
 };
 
